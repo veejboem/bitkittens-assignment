7a0de7a4d4db044ab6c72891f5a8974aa15eda11
Valerie Boem <vboem@gmail.com>
2017-02-18 20:33:17 -0500

    Fixed to add one img to div and added alt attribute

--- cats.js
+++ cats.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
     method: 'GET',
     url: 'http://bitkittens.herokuapp.com/cats.json',
     dataType:'JSON',
-  }).done(function (data) {
+    }).done(function (data) {
 
-    for(var i=0; i < data.cats.length; i++) {
-      $('.cat-box').append('<img src= "' + data.cats[i].photo + '">');
-      // $('src').append('<alt = "Photo of:' + data.cats[i].name + '"' );
-    }
-
+      for(var i=0; i < data.cats.length; i++) {
+        $('<img>')
+          .attr('src', data.cats[i]['photo'])
+          .attr('alt', 'Photo of: ' + data.cats[i]['name'])
+          .appendTo('#cat' + (i + 1) )
+      ;}
     });
   });
 });
