078d699ae1123d41984e606e6982e09212163ee0
Valerie Boem <vboem@gmail.com>
2017-02-18 19:42:39 -0500

    Adding cats to the DOM partially complete. Ajax request set up.

--- cats.js
+++ cats.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
   $('.summon-cats').click(function() {
-    console.log('gathering felines');
+    $.ajax({
+    method: 'GET',
+    url: 'http://bitkittens.herokuapp.com/cats.json',
+    dataType:'JSON',
+  }).done(function (data) {
+
+    for(var i=0; i < data.cats.length; i++) {
+      $('.cat-box').append('<img src= "' + data.cats[i].photo + '">');
+      // $('src').append('<alt = "Photo of:' + data.cats[i].name + '"' );
+    }
+
+    });
   });
 });
